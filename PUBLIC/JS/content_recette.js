3a22cfc915b007ad0a46bac0426984acc5c69b45
kevinhuron <kevin.huron.sio@gmail.com>
2016-01-20 09:05:34 +0100

    add slider for show content recette, beginning content recette

--- PUBLIC/JS/content_recette.js
+++ PUBLIC/JS/content_recette.js
@@ -3,6 +3,38 @@
  */
 
 $(document).ready(function(){
+    var jssor_1_options = {
+        $AutoPlay: true,
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$
+        },
+        $ThumbnailNavigatorOptions: {
+            $Class: $JssorThumbnailNavigator$,
+            $Cols: 3,
+            $Align: 200
+        }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    //responsive code begin
+    //you can remove responsive code if you don't want the slider scales while window resizing
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 600);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    //responsive code end
+
 
     var cle = $("#id_r").html();
     var rq = $.ajax({
@@ -16,21 +48,31 @@ $(document).ready(function(){
 
         var all_title = content_recette['content_recette'];
         var img;
-        var silder = $(".has-slider");
-        var silder_item = $("#c_item");
+        var slider = $("#theslider");
         var i = 1;
 
         $.each(all_title, function (key, value) {
             img = value['name_img'];
-            silder.before('<li data-target="#carousel-example-generic" data-slide-to="'+i+'"></li>')
-            silder_item.append('<div class="carousel-item"><img src="PUBLIC/IMG/'+img+'" alt="slide n°'+i+'"><div class="carousel-caption"><h3>'+value['title']+'</h3><p>'+value['type_dish']+'</p></div></div>')
+            slider.append('<div data-p="112.50" style="display: none;">' +
+                '<img data-u="image" src="PUBLIC/IMG/img" />' +
+                '<div data-u="thumb">' +
+                '<img src="PUBLIC/IMG/recette1.jpg" />' +
+                '<div class="title_back"></div>' +
+                '<div class="title">' +
+                '</div>' +
+                '</div>' +
+                '</div>');
 
-            $('#id_r').append('<img src="PUBLIC/IMG/' + img + '" alt="img" style="width: 100%;height: 100%;" />' +
-                '<h4 class="card-title">' + value['title'] + '</h4>' +
-                '<p class="card-text"><strong>' + value['type_dish'] + '</strong><br>' +
-                'Difficulté : ' + value['difficulty'] + '<br>' +
-                'Coût : ' + value['cost'] + '<br>' +
-                'Portions : ' + value['nb_port'] + ' </p>');
+            $('#titre_r').append(value['title']);
+            $('#categ_r').append(value['name_c']);
+            $('#type_dish_r').append(value['type_dish']);
+            $('#diff_r').append(value['difficulty']);
+            $('#cost_r').append(value['cost']);
+            $('#tmp_prep_r').append(value['time_prep']);
+            $('#tmp_cook_r').append(value['cook_time']);
+            $('#test').append(value['']);
         });
     });
+
+
 });
